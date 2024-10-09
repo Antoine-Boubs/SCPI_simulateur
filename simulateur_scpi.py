429a22bc3064452a19d9987dda6e02e741f1a644
Antoine Geek <antoine.berjoan+github@hotmail.fr>
2024-10-09 16:20:10 +0000

    Mise à jour de l'application avec les dernières modifications

--- simulateur_scpi.py
+++ simulateur_scpi.py
@@ -580,8 +580,7 @@ def plot_amortissement(df_amortissement, df_investissement, duree_pret, apport):
             traceorder="normal",
             font=dict(size=10),
             itemsizing="constant",
-            itemwidth=50,
-            width=3
+            itemwidth=150,
         )
     )
 
